f3932aa30638e61a0abf3dbdfc9b7beafec16135
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2024-01-08 21:18:56 +0100

    made some basic interaction functionality

---
+++ src/objects/pickupeble_and_takable.gd
@@ -0,0 +1,28 @@
+extends RigidBody3D
+
+var interacter: Object = null
+
+func _physics_process(delta):
+	if interacter != null:
+		interact()
+
+func start_interact(new_interacter: Object) -> bool:
+	if Input.is_action_pressed("pick_up"):
+		interacter = new_interacter
+		return true
+	
+	if Input.is_action_just_pressed("take"):
+		queue_free()
+	
+	return false
+
+func interact() -> void:
+	if Input.is_action_pressed("pick_up"):
+		gravity_scale = 0.0
+		linear_velocity = (interacter.hand.global_position - global_position)*1000.0*get_physics_process_delta_time()
+	else:
+		end_interaction()
+		gravity_scale = 1.0
+	
+func end_interaction() -> void:
+	interacter = null
---
+++ src/objects/takable.gd
@@ -0,0 +1,14 @@
+extends Node3D
+	
+var interacter: Object = null
+	
+func start_interact(new_interacter: Object) -> bool:
+	if Input.is_action_just_pressed("take"):
+		queue_free()
+	return false
+	
+func interact() -> void:
+	pass
+
+func end_interaction() -> void:
+	interacter = null
