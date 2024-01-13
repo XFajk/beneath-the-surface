5868bf52aaef862975023e2b3191b7cf498b34ae
Jozef Šifra <70467972+juzek32@users.noreply.github.com>
2024-01-14 00:25:58 +0100

    functioning item generation and music player
    everything works but still need models and lighting and doors are teporarly removed for easier level design can be added back by toggling their visibility in their tscn file

--- src/level/level.gd
+++ src/level/level.gd
@@ -2,16 +2,26 @@ extends Node3D
 
 @export var small_item_count: int = 5
 @export var medium_item_count: int = 5
-@export var large_item_count: int = 5
+@export var large_item_count: int = 2
 
 var small_positions: Array
 var medium_positions: Array
 var large_positions: Array
 
-var small_items: Array = [preload("res://assets/Phone.tscn"), preload("res://assets/Console.tscn")]
+var small_items: Array = [preload("res://assets/Phone.tscn"), preload("res://assets/Ring.tscn"), 
+	preload("res://assets/Wallet.tscn"), preload("res://assets/Money_Pile.tscn")]
+
+var medium_items: Array = [preload("res://assets/Laptop.tscn"), preload("res://assets/Console.tscn")]
+
+var large_items: Array = [preload("res://assets/Vase.tscn"), preload("res://assets/Console.tscn")]
+
+@onready var music: AudioStreamPlayer = $AudioStreamPlayer
+var time = 45
 
 func _ready():
 	randomize()
+	
+	music.volume_db = -80
 	
 	var tree = get_tree()
 	
@@ -25,13 +35,33 @@ func _ready():
 		small_positions.erase(pos_node)
 		
 		var pos: Vector3 = pos_node.global_position
-		print(pos_node.global_position)
-		print(pos)
 		add_child(item)
 		item.global_position = pos
-		print(item.global_position)
-		print("------")
+		item.rotation.y = randf_range(-180, 180)
+	
+	for i in range(medium_item_count):
+		var item = medium_items[randi() % medium_items.size()].instantiate()
+		var pos_node: Node3D = medium_positions[randi() % medium_positions.size()]
+		medium_positions.erase(pos_node)
+		
+		var pos: Vector3 = pos_node.global_position
+		add_child(item)
+		item.global_position = pos
+		item.rotation.y = randf_range(-180, 180)
+		
+	for i in range(large_item_count):
+		var item = large_items[randi() % large_items.size()].instantiate()
+		var pos_node: Node3D = large_positions[randi() % large_positions.size()]
+		large_positions.erase(pos_node)
+		
+		var pos: Vector3 = pos_node.global_position
+		add_child(item)
+		item.global_position = pos
+		item.rotation.y = randf_range(-180, 180)
+		
+	music.play(93 - time)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if music.volume_db < -2:
+		music.volume_db += 33 * delta
